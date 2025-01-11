edd421f30a7575ed5bba4596e0320b1234f7f392
Ezri Benami <ezricissp@gmail.com>
2025-01-11 18:18:03 -0500

    Update 8.py

--- 2024-04-01-intro-dev-python/8.py
+++ 2024-04-01-intro-dev-python/8.py
@@ -1,6 +1,6 @@
 from ollama import Client
 
-client = Client(host="http://mattsremoteollamaapi:11434")
+client = Client(host="http://localhost:11434")
 prompt = "why is the sky blue"
 
 output = client.generate(model="llama3.2", prompt=prompt)
